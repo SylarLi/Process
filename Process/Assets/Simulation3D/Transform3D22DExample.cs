0c589d605d56ce8d9ec83611e5cecb4d2cf2e241
SylarLi <328403302@qq.com>
2015-04-10 19:59:08 +0800

    2D平面模拟跳跃等3D内容

---
+++ Process/Assets/Simulation3D/Transform3D22DExample.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transform3D22DExample : MonoBehaviour
+{
+    private float projectY = float.NaN;
+
+    private float airTime = 0;
+
+    private float airSlideH = 0;
+
+    private float airSlideV = 0;
+
+    public float gravity = -9.8f;
+
+    public float jumpStartSpeed = 3f;
+
+    public float dirSpeedH = 1f;
+
+    public float dirSpeedV = 1f;
+
+    private Vector3 mPosition;
+
+    private GameObject shadow;
+
+    void Start()
+    {
+        mPosition = transform.position;
+        shadow = GameObject.Find("Shadow");
+    }
+
+    private void FixedUpdate()
+    {
+        if (airTime > 0)
+        {
+            rigidbody2D.AddForce(new Vector2(0, gravity * rigidbody2D.mass), ForceMode2D.Force);
+        }
+    }
+
+    private void Update()
+    {
+        if (airTime > 0)
+        {
+            if (airSlideH == 0 && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    airSlideH = -1;
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    airSlideH = 1;
+                }
+                rigidbody2D.velocity += new Vector2(airSlideH * dirSpeedH, 0);
+            }
+            if (airSlideV == 0 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+            {
+                if (Input.GetKey(KeyCode.S))
+                {
+                    airSlideV = -1f;
+                }
+                else if (Input.GetKey(KeyCode.W))
+                {
+                    airSlideV = 1f;
+                }
+                rigidbody2D.velocity += new Vector2(0, airSlideV * dirSpeedV);
+            }
+            if (airSlideV != 0)
+            {
+                projectY += airSlideV * dirSpeedV * Time.deltaTime;
+            }
+            position = TransformUtil.Position2DY23DZ(transform.position, projectY);
+            if ((airTime -= Time.deltaTime) <= 0)
+            {
+                airTime = 0;
+                airSlideH = 0;
+                airSlideV = 0;
+                rigidbody2D.velocity = Vector2.zero;
+                position = new Vector3(position.x, position.y, 0);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.J))
+            {
+                airTime = Mathf.Abs(jumpStartSpeed / gravity) * 2;
+                projectY = position.y;
+                rigidbody2D.velocity += new Vector2(0, jumpStartSpeed);
+            }
+        }
+        shadow.transform.position = new Vector3(position.x, position.y, 0);
+    }
+
+    public Vector3 position
+    {
+        get
+        {
+            return mPosition;
+        }
+        set
+        {
+            if (mPosition != value)
+            {
+                mPosition = value;
+                transform.position = TransformUtil.Position3DZ22DY(mPosition);
+            }
+        }
+    }
+}
--- Process/Assets/Simulation3D/TransformUtil.cs
+++ Process/Assets/Simulation3D/TransformUtil.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class TransformUtil 
 {
@@ -10,9 +9,22 @@ public class TransformUtil
     /// <param name="origin"></param>
     /// <param name="angle">0 <= camera euler angle x < 90</param>
     /// <returns></returns>
-    public static Vector2 Position3DZ22DY(Vector3 origin, float angle = 45)
+    public static Vector3 Position3DZ22DY(Vector3 origin, float angle = 45)
     {
         return new Vector3(origin.x, origin.y + origin.z * Mathf.Tan((90 - angle) * Mathf.PI / 180), 0);
+    }
+
+    /// <summary>
+    /// 正交坐标转为透视坐标
+    /// 将z坐标从y中分离出来
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="y"></param>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    public static Vector3 Position2DY23DZ(Vector3 origin, float y, float angle = 45)
+    {
+        return new Vector3(origin.x, y, (origin.y - y) / Mathf.Tan((90 - angle) * Mathf.PI / 180));
     }
 
     /// <summary>
